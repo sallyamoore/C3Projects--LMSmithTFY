c17efa738e1efdaf7bc36fbb13606ba6dcc40512
Sally Moore <sallyamoore@gmail.com>
2015-09-05 15:11:54 -0700

    Generated songs controller. Added initial specs, then #index method.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+require 'httparty'
+
+class SongsController < ApplicationController
+
+  CALLBACK_URL = "http://localhost:3000/callback/"
+  def index; end
+end
---
+++ spec/controllers/songs_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe SongsController, type: :controller do
+  describe "GET #index" do
+    it "should be successful" do
+      get :index
+      expect(response).to be_ok
+    end
+  end
+
+  describe "Searching the db for relevant lyrics" do
+    describe "GET #search" do
+      it "should be successful" do
+        get :search
+        expect(response).to be_ok
+      end
+    end
+  end
+
+  # describe "Querying the API", vcr: song_cassette do
+  #   before :each do
+  #     get :api_query, song: "XXX"
+  #   end
+  #
+  #   it "should be successful" do
+  #     expect(response).to be_ok
+  #   end
+  #
+  #   it "should return a json response object" do
+  #     expect(response.header['Content-Type']).to include 'application/json'
+  #   end
+  #
+  #   context "the returned json object" do
+  #     it "has the expected keys" do
+  #       data = JSON.parse response.body
+  #
+  #       %w(title artist via url).each do |key|
+  #         expect(data.map(&:keys).flatten.uniq).to include key
+  #       end
+  #     end
+  #   end
+  # end
+end
